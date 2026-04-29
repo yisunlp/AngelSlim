e1b70964fe33fc3bfb93db2d0fdea6476103048f
root <root@ts-8b1d89fa9d436317019d48213b8604f9-launcher.ts-8b1d89fa9d436317019d48213b8604f9.taiji-hyaide-compress-nj-h800h-zhongwei-1.svc.cluster.local>
2026-04-29 13:44:03 +0800

    fix pre-commit

--- angelslim/utils/__init__.py
+++ angelslim/utils/__init__.py
@@ -30,4 +30,4 @@ from .utils import print_info  # noqa: F401
 from .utils import print_with_rank  # noqa: F401
 from .utils import rank0_print  # noqa: F401
 from .utils import set_op_by_name  # noqa: F401
-from .zero3_io import *  # noqa: F401 403
+from .zero3_io import *  # noqa: F401 F403
